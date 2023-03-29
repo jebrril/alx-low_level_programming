50b5094aff2ccf49726755a62adda5c59a3b4122
jebrril <125573348+jebrril@users.noreply.github.com>
2023-03-30 00:55:49 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,26 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _strncpy - Copies a string
- * @dest: Destination string
- * @src: Source string
- * @n: Number of characters to copy
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
  *
- * Return: Pointer to destination string
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int index = 0, src_len = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	while (src[index++])
+		src_len++;
 
-	for (; i < n; i++)
-		dest[i] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
 	return (dest);
 }
