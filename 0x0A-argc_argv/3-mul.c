bcf1a228393dffb4537a6e458cbbf00cd825d6fd
jebrril <125573348+jebrril@users.noreply.github.com>
2023-04-11 05:18:44 +0200

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies only two numbers.
+ * incase you gave up to 3 numbers the output: Error
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char  **argv)
+{
+	int i, sum;
+
+	if (argc != 3)
+		{
+		printf("Error\n");
+		return (1);
+		}
+	if (argc > 1)
+	{
+		sum = atoi(argv[1]);
+		for (i = 2; i < argc; i++)
+		{
+			sum = sum * atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+return (0);
+}
