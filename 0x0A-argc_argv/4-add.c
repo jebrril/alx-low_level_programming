017dea15c85e5aab467d53951bb2a36fa72c7e80
jebrril <125573348+jebrril@users.noreply.github.com>
2023-04-11 05:20:46 +0200

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+/**
+ * main - add up two numbers.
+ * incase you gave character or odd symble the output: Error and
+ * Return 1
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char  **argv)
+{
+	int a, b, x, sum = 0;
+	bool flag = false;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		flag = true;
+	}
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		x = strtol(argv[a], NULL, 10);
+		sum = sum + x;
+	}
+	if (!flag)
+		printf("%d\n", sum);
+	return (0);
+}
