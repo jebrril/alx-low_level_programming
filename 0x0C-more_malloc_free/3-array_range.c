b467c22d761f351e7c770ad04ed689ac22df69e9
jebrril <125573348+jebrril@users.noreply.github.com>
2023-04-14 15:06:12 +0000

    Create 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/*BY-JEBRIIIL*/
+/**
+ *array_range -function that creates an array of integers.
+ *@min: value 1
+ *@max: value 2
+ *Return: array
+*/
+int *array_range(int min, int max)
+{
+	int i = 0;
+	int sum = 0;
+	int *matrix;
+
+	if (min > max)
+		return (NULL);
+	sum = (max - min) + 1;
+
+	matrix = (int *) malloc(sum * sizeof(int));
+
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i < sum; i++)
+		matrix[i] = min + i;
+	return (matrix);
+}
