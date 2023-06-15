15a560bff875bcfb6603ee887ff637f25f40e32c
jebrril <125573348+jebrril@users.noreply.github.com>
2023-06-15 22:33:58 +0100

    Create 6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* sum_dlistint - pritnt the total of the nodes
+* @head: the head of dlist
+* Return: the total
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int total = 0;
+	dlistint_t *op = head;
+
+	while (op != NULL)
+	{
+		total += op->n;
+		op = op->next;
+	}
+	return (total);
+}
